fd67baa40807b2819d8823e48c57f5551012b15a
Shubham Goyal <53435671+Shubhamgoyal26@users.noreply.github.com>
2021-03-04 21:34:17 +0530

    Create voteEligibiltiy.cpp

---
+++ voteEligibiltiy.cpp
@@ -0,0 +1,25 @@
+// Checking eligibility for voting
+
+#include<iostream>
+#include<iso646.h>
+
+using namespace std;
+
+int main(void){
+    int age;
+    
+    cout<<"Enter your age: ";
+    cin>>age;
+
+    if(age >= 18){
+        cout<<"You are eligible to vote";
+    }
+    else if(age < 0){
+        cout<<"Age cannot be negative";
+    }
+    else{
+        cout<<"You are not eligible to vote"<<endl<<"Wait for "<<18 - age<<" years to vote";
+    }
+
+    return 0;
+}
