8673dc9bc2fc01e959cfec7afe16c82248c51a4b
Shubham Goyal <53435671+Shubhamgoyal26@users.noreply.github.com>
2021-03-04 21:15:46 +0530

    Create product.c

---
+++ product.c
@@ -0,0 +1,17 @@
+// Finding product of two numbers
+
+#include<iostream>
+
+using namespace std;
+
+int main(void){
+    int num1, num2, product;
+    cout<<"Enter first number: ";
+    cin>>num1;
+    cout<<"Enter second number: ";
+    cin>>num2;
+    product = num1 * num2;
+    cout<<"Product = "<< product;
+
+    return 0;
+}
